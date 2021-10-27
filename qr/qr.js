7905bcec560d4c13aca31308cb6a4b448148310e
blucubed <patrick@arutunian.com>
2021-10-26 19:34:40 -0700

    must return value

--- qr/qr.js
+++ qr/qr.js
@@ -17,7 +17,9 @@ Http.send(JSON.stringify({ "poster": poster}));
 
 Http.onreadystatechange = (e) => {
   console.log(e.currentTarget.status)
+
+  if (poster && redirects[poster]) window.location.replace(redirects[poster])
+  else window.location.replace(redirects.default)
 }
 
-if (poster && redirects[poster]) window.location.replace(redirects[poster])
-else window.location.replace(redirects.default)
+
